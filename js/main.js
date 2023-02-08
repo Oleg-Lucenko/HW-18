bcf35673fe9ee9d0dc301f184e5cb9cef92de8dd
Oleg-Lucenko <lucenko5577@gmail.com>
2023-02-08 11:46:42 +0200

    HW 18 Lucenko

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let image = document.querySelector('.image');
 
-let imgArr = ['images/img1.jpeg','images/img2.jpeg','images/img3.jpeg','images/img4.jpeg','images/img5.jpeg','images/img6.jpeg','images/img7.jpeg','images/img8.jpeg', 'images/img9.jpeg'];
+let imgArr = ['img/img1.jpeg','img/img2.jpeg','img/img3.jpeg','img/img4.jpeg','img/img5.jpeg','img/img6.jpeg','img/img7.jpeg','img/img8.jpeg', 'img/img9.jpeg'];
 
 
 image.src = imgArr[Math.floor(Math.random() * imgArr.length)];
